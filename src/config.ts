92ae62fd68fb31be7723fea91e7b2ed9c60fb0d0
ptminh-kmp <ptminh.gun@gmail.com>
2026-04-08 17:31:13 +0700

    chore: update social links to point to personal profiles and add LinkedIn

--- src/config.ts
+++ src/config.ts
@@ -47,7 +47,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Product,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/ptminh-kmp", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -59,22 +59,15 @@ export const profileConfig: ProfileConfig = {
 	bio: "My personal blog",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
-		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/ptminh-kmp",
 		},
+		{
+			name: "LinkedIn",
+			icon: "fa6-brands:linkedin",
+			url: "https://www.linkedin.com/in/ptminh-kmp/",
+		}
 	],
 };
 
